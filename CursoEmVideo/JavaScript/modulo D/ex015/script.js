e84cc3de9a71ed58e9319a3b6bdcfd0268ba6fa1
Isaac Romero <is4.romero@gmail.com>
2024-03-23 12:35:07 -0300

    uptade

--- CursoEmVideo/JavaScript/modulo D/ex015/script.js
+++ CursoEmVideo/JavaScript/modulo D/ex015/script.js
@@ -2,24 +2,28 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
+
     var resp = window.document.getElementById('res')
+    var resptxt = window.document.getElementById('resptxt')
+
     if (fano.value.length = 0 || Number(fano.value) > ano) {
         window.alert('Verifique os dados e tente novamente')
+
     } else {
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = window.document.createElement('img')
         img.setAttribute('id', 'foto')
+
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade < 10) {
-                // Criança
-                //img.setAttribute('src', 'nome do arquivo.png ou jpg')
+                img.setAttribute('src', 'crianca-h-250.jpg')
             } else if (idade < 21) {
-                // Adulto
+                img.setAttribute('src', 'adulto-h-250.jpg')
             } else {
-                // Idoso
+                img.setAttribute('src', 'idoso-h-250.jpg')
             }
         } else {
             genero = 'Mulher'
@@ -31,7 +35,8 @@ function verificar() {
                 // Idoso
             }
         }
-        resp.style.textAlign = 'center'
-        resp.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        resptxt.style.textAlign = 'center'
+        resptxt.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        resp.appendChild(img)
     }
 }
